8a3a89efd0eb4220fee7c0c76d022bcd9d1ca9d1
Daniel Sousa <sousa.danielg@gmail.com>
2017-02-13 20:10:32 -0800

    issue

--- app/index.jsx
+++ app/index.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import 'bootstrap-loader';
 import './scss/application.scss';
-//import Application from './components/Application.jsx';
-//import Data from './data.json'
+import Application from './components/Application.jsx';
+import Data from './data.json'
 
 
 
 
-//ReactDOM.render(<Application data={}/>, document.getElementById('app'));
+ReactDOM.render(<Application data={Data}/>, document.getElementById('app'));
 
-ReactDOM.render(<div>Hello World!</div>, document.getElementById('app'))
