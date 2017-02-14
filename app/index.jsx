21cb00ca967c37b5193fdf4e9965ea8412fc63ff
Daniel Sousa <sousa.danielg@gmail.com>
2017-02-13 22:51:56 -0800

    check

--- app/index.jsx
+++ app/index.jsx
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom";
 import 'bootstrap-loader';
 import './scss/application.scss';
 import Application from './components/Application.jsx';
-import Data from './data.json';
+// import Data from './data.json';
 
-var data = function() {
-	if(localStorage._recipes) {
-		return JSON.parse(localStorage._recipes)
-	} else {
-		return Data.recipes
-	}
-}
+// var data = function() {
+// 	if(localStorage._recipes) {
+// 		return JSON.parse(localStorage._recipes)
+// 	} else {
+// 		return Data.recipes
+// 	}
+// }
 
-ReactDOM.render(<Application data={data()}/>, document.getElementById('app'));
+ReactDOM.render(<Application data={JSON.parse(localStorage._recipes)}/>, document.getElementById('app'));
 
